e0ddd72330854d895881d898b0ecc07d5e4501de
pauloosmt <paulostaciano@gmail.com>
2026-03-17 20:16:55 -0300

    feat: UserRepository created

---
+++ src/main/java/com/br/zetta/fire/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.br.zetta.fire.repository;
+
+import com.br.zetta.fire.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}

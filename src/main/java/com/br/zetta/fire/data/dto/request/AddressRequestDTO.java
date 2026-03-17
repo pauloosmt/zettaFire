b64082530805ae1c70c6e350032431c1d3dc918d
pauloosmt <paulostaciano@gmail.com>
2026-03-16 23:05:18 -0300

    feat: Address entity created

---
+++ src/main/java/com/br/zetta/fire/data/dto/request/AddressRequestDTO.java
@@ -0,0 +1,33 @@
+package com.br.zetta.fire.data.dto.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record AddressRequestDTO(
+        @NotBlank(message = "CEP is required")
+        @Pattern(regexp = "\\d{8}", message = "Invalid CEP")
+        String cep,
+
+        @NotBlank(message = "Street is required")
+        String street,
+
+        String number,
+
+        @NotBlank(message = "District is required")
+        String district,
+
+        @NotBlank(message = "City is required")
+        String city,
+
+        @NotBlank(message = "State is required")
+        String state,
+
+        @NotBlank(message = "Latitude is required")
+        Double latitude,
+
+        @NotBlank(message = "Longitude is required")
+        Double longitude
+
+) {
+}

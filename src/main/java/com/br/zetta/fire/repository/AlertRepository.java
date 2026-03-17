c1b34d0f374411301a31753744c7f5dabefae328
pauloosmt <paulostaciano@gmail.com>
2026-03-17 20:16:41 -0300

    feat: AlertRepository created

---
+++ src/main/java/com/br/zetta/fire/repository/AlertRepository.java
@@ -0,0 +1,18 @@
+package com.br.zetta.fire.repository;
+
+
+import com.br.zetta.fire.data.entity.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, UUID> {
+
+    List<Alert> findByUser(UUID idUser);
+
+    List<Alert> findByFireEvent(UUID idFireEvent);
+
+}

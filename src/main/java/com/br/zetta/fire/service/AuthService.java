22a986085953293c5d3fac30e080c24d983a6f8c
pauloosmt <paulostaciano@gmail.com>
2026-03-24 22:24:01 -0300

    feat: AuthService created

---
+++ src/main/java/com/br/zetta/fire/service/AuthService.java
@@ -0,0 +1,23 @@
+package com.br.zetta.fire.service;
+
+import com.br.zetta.fire.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+    final
+    UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
+}

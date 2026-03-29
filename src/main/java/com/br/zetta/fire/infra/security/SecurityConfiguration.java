9ce76d6397ca991c0d94ba4c8e300d40b1477e7d
pauloosmt <paulostaciano@gmail.com>
2026-03-29 00:04:26 -0300

    feat: authentication system completion

--- src/main/java/com/br/zetta/fire/infra/security/SecurityConfiguration.java
+++ src/main/java/com/br/zetta/fire/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.br.zetta.fire.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,11 +10,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    final
+    SecurityFilter securityFilter;
+
+    public SecurityConfiguration(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         return httpSecurity
@@ -23,11 +34,17 @@ public class SecurityConfiguration {
                                 .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
                                 .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
     @Bean
-    public AuthenticationManager authManager(AuthenticationConfiguration authenticationConfiguration)  throws Exception{
+    public AuthenticationManager authManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

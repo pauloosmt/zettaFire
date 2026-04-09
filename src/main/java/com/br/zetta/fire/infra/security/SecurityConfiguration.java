e3462681e761810bfb86e9eaa082db9debd6be9e
pauloosmt <paulostaciano@gmail.com>
2026-04-09 00:11:50 -0300

    feat: messaging system implemented, test version

--- src/main/java/com/br/zetta/fire/infra/security/SecurityConfiguration.java
+++ src/main/java/com/br/zetta/fire/infra/security/SecurityConfiguration.java
@@ -33,6 +33,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/alert/send").permitAll()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();

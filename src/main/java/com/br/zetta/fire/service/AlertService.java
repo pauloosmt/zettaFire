63cba153555b066610a8ea74511586458ff7e4b9
pauloosmt <paulostaciano@gmail.com>
2026-04-15 15:30:19 -0300

    fix: converting response from string to json

--- src/main/java/com/br/zetta/fire/service/AlertService.java
+++ src/main/java/com/br/zetta/fire/service/AlertService.java
@@ -1,13 +1,17 @@
 package com.br.zetta.fire.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class AlertService {
     private final JavaMailSender mailSender;
+    private static final Logger logger = LoggerFactory.getLogger(AlertService.class);
 
     //Mensagens padrões alertas
     private static final String SUBJECT = "️ ALERTA CRÍTICO: Risco de Incêndio Detectado";
@@ -20,16 +24,26 @@ public class AlertService {
             Este é um alerta automático. Não responda a este e-mail.
             """;
 
-    public AlertService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
+    @Async
+    public void sendEmail(String to) {
+        try {
+            logger.info("Iniciando tenatativa de envio de alerta de incêndio para: {}", to);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(SUBJECT);
+            message.setText(BODY_TEMPLATE);
+
+            mailSender.send(message);
+
+            logger.info("Alerta enviado com sucesso para: {}", to);
+
+        } catch (Exception e) {
+            logger.error("ERRO CRITICO: Falha ao enviar alerta para {}. Motivo: {}", to, e.getMessage());
+        }
     }
 
-    public void sendEmail(String to) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(SUBJECT);
-        message.setText(BODY_TEMPLATE);
-
-        mailSender.send(message);
+    public AlertService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
 }
